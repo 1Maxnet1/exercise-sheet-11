21a55f0b190045179790911205774a352869c393
Max Buchholz <Max.Buchholz@gmx.de>
2021-02-11 17:22:17 +0100

    Implement methods in HamsterClub

--- src/main/java/de/unistuttgart/iste/sqa/pse/exercisesheet11/streams/HamsterClub.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/exercisesheet11/streams/HamsterClub.java
@@ -2,65 +2,113 @@ package de.unistuttgart.iste.sqa.pse.exercisesheet11.streams;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Write a description for your HamsterClub class.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Max Buchholz, Jannik Funk
+ * @version 11.02.2021
  */
 public class HamsterClub implements Club {
 
-	private final List<ClubMember> members;
-	private final Comparator<ClubMember> clubComparator;
-	
-	public HamsterClub(final List<ClubMember> members) {
-		if(members != null) {
-			this.members = members;
-		} else {
-			throw new IllegalArgumentException();
-		}
-		
-		this.clubComparator = generateComparator();
-	}
-	
-	
-	
-	@Override
-	public List<String> getNameOfMembers() {
-		return null;
+    private final List<ClubMember> members;
+    private final Comparator<ClubMember> clubComparator;
+
+    /**
+     *
+     * @param members
+     */
+    /*
+     *@ requires
+     *@ ensures
+     */
+    public HamsterClub(final List<ClubMember> members) {
+	if (members != null) {
+	    this.members = members;
+	} else {
+	    throw new IllegalArgumentException();
 	}
 
-	@Override
-	public Double getTotalContributions() {
-		return 0.0;
-	}
+	this.clubComparator = generateComparator();
+    }
 
-	@Override
-	public void applyDiscount() {
-	
-	}
+    /**
+     * Get the List of all Names of the ClubMembers
+     * @return List with all members
+     */
+    /*
+     *@ requires
+     *@ ensures
+     */
+    @Override public List<String> getNameOfMembers() {
+	return members.stream().map(ClubMember::getName).collect(Collectors.toList());
+    }
 
-	@Override
-	public List<ClubMember> getMembersWithUnpaidFees() {
-		return null;
-	}
+    /**
+     *
+     * @return List with
+     */
+    /*
+     *@ requires
+     *@ ensures
+     */
+    @Override public Double getTotalContributions() {
+	return members.stream().mapToDouble(ClubMember::getContributions).sum();
+    }
 
-	/**
-	 * implement your Comparator here
-	 */
-	private Comparator<ClubMember> generateComparator() {
-		return null;
-	}
+    /**
+     *
+     */
+    /*
+     *@ requires
+     *@ ensures
+     */
+    @Override public void applyDiscount() {
+	members.stream().filter(b -> b.getAge() > 60).forEach(c->c.setContributions(c.getContributions()*0.9));
+    }
 
-	@Override
-	public ClubMember getOldestMember() {
-		return null;
-	}
-	
-	@Override
-	public List<ClubMember> getMembers(){
-		return members;
-	}
+    /**
+     *
+     * @return
+     */
+    @Override public List<ClubMember> getMembersWithUnpaidFees() {
+	return members.stream().filter(b -> !b.getHasPaidContributions()).collect(Collectors.toList());
+    }
+
+    /**
+     * implement your Comparator here
+     */
+    /*
+     *@ requires
+     *@ ensures
+     */
+    private Comparator<ClubMember> generateComparator() {
+	return null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    /*
+     *@ requires
+     *@ ensures
+     */
+    @Override public ClubMember getOldestMember() {
+	return null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    /*
+     *@ requires
+     *@ ensures
+     */
+    @Override public List<ClubMember> getMembers() {
+	return members;
+    }
 
 }
